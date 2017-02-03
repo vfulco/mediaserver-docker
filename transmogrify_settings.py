2e8b47c9aeb1557d50b9b70b5e5f54e8b4ab9d2d
Corey Oordt <coreyoordt@gmail.com>
2017-02-03 11:21:33 -0600

    Allow transmogrify_settings.py to get some config from the environment.

--- transmogrify_settings.py
+++ transmogrify_settings.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 
 NO_IMAGE_URL = ''
-ORIG_BASE_PATH = '/code/originals'
-BASE_PATH = '/code/modified'
+ORIG_BASE_PATH = os.path.join(os.environ.get('HOMEDIR', '/etc/transmogrify'), 'originals')
+BASE_PATH = os.path.join(os.environ.get('HOMEDIR', '/etc/transmogrify'), 'modified')
 DEBUG = True
 SECRET_KEY = 'n*d%%vr4a3^a0d=gl+vyq$0xfp#=ial9n#-)+*csvctly&fvek'
 PATH_ALIASES = {'^assets/': '/'}
